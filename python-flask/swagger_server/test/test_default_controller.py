bf609c4a77e27e9da9c002dcbd9cab01090ea101
apurvnnd <apurv@blueed.com>
2019-08-26 11:01:13 +0000

    Generated code by SwaggerHub

--- python-flask/swagger_server/test/test_default_controller.py
+++ python-flask/swagger_server/test/test_default_controller.py
@@ -6,13 +6,29 @@ from flask import json
 from six import BytesIO
 
 from swagger_server.models.body import Body  # noqa: E501
+from swagger_server.models.body1 import Body1  # noqa: E501
 from swagger_server.models.inline_response200 import InlineResponse200  # noqa: E501
 from swagger_server.models.inline_response2001 import InlineResponse2001  # noqa: E501
+from swagger_server.models.inline_response2002 import InlineResponse2002  # noqa: E501
 from swagger_server.test import BaseTestCase
 
 
 class TestDefaultController(BaseTestCase):
     """DefaultController integration test stubs"""
+
+    def test_add_notification_post(self):
+        """Test case for add_notification_post
+
+        Add new Notification
+        """
+        body = Body1()
+        response = self.client.open(
+            '/Json/addNotification',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
 
     def test_get_event_get(self):
         """Test case for get_event_get
